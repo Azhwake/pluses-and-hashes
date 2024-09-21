6c6272ec77cd21bce7dc515b316c8899bcaee2a9
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:19:55 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // Good Luck 💪🏾
+function plusHashCount(str) {
+    let hashCount = 0;
+    let plusCount = 0;
+
+    for (let char of str) {
+        if (char === '#') {
+            hashCount++;
+        } else if (char === '+') {
+            plusCount++;
+        }
+    }
+
+    return [hashCount, plusCount];
+}
+
+// Test cases
+console.log(plusHashCount("###+"));        // ➞ [3, 1]
+console.log(plusHashCount("##+++#"));      // ➞ [3, 3]
+console.log(plusHashCount("#+++#+#++#"));  // ➞ [6, 4]
+console.log(plusHashCount(""));            // ➞ [0, 0]
